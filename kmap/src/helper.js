5374d92a05cc290a561c081e70638aaa4e55eecf
ChristopherLit <litchristopher@gmail.com>
2023-11-18 17:11:42 -0500

    Bug fixes

--- kmap/src/helper.js
+++ kmap/src/helper.js
@@ -1,97 +1,139 @@
   //SOP
-  //implicant = [[1,2], [1,3]]
+  //implicant = [1, 2, 3]
   //numVariables = 3,4
   function findEquation(implicant, numVariables) {
 
     const values = new Array(implicant.length);
     let tempValue = "";
-    let compare = "";
     let answer = "";
+    let compare = "";
+    let different = false;
 
     for (let i = 0; i < implicant.length; i++) {
 
-        tempValue = equation(implicant[i]);
+        tempValue = equation(implicant[i], numVariables);
         values[i] = tempValue;
 
 
     }
 
-    if (numvariables == 3) {
+    if (numVariables == 3) {
 
-        for (let a = 0; a < values.length; a++) {
+        if (values.length == 8) {
 
-            const first = values[0];
-            const second = values[1];
-            const third = values[2];
+            return "1";
 
-            if (first[a] == second[a] && second[a] == third[a]) {
+        }
 
-                if (first[a] == "E") {
+        for (let b = 0; b < 3; b++) {
+
+            const firstString = values[0];
+            compare = firstString[b];
+
+            for (let a = 1; a < values.length; a++) {
+
+                const nextString = values[a];
+
+                if (nextString[b] != compare) {
+
+                    different = true;
+                    break;
+
+                    
+                }  
+            }
+
+            if (different == false) {
+
+                if (firstString[b] == "E") {
 
                     answer += "A'";
 
-                } else if (first[a] == "F") {
+                } else if (firstString[b] == "F") {
 
                     answer += "B'";
 
 
-                } else if (first[a] == "G") {
+                } else if (firstString[b] == "G") {
                     
                     answer += "C'";
 
 
                 } else {
 
-                    answer += first[a];
+                    answer += firstString[b];
 
                 }
             }
-        }
+
+            different = false;
+    }
 
     } else if (numVariables == 4) {
 
-        for (let a = 0; a < values.length; a++) {
+        if (values.length == 16) {
 
-            const first = values[0];
-            const second = values[1];
-            const third = values[2];
-            const fourth = values[3];
+            return "1";
+            
+        }
 
-            if (first[a] == second[a] && second[a] == third[a] && third[a] == fourth[a]) {
+        for (let b = 0; b < 4; b++) {
 
-                if (first[a] == "E") {
+            const firstString = values[0];
+            compare = firstString[b];
+
+            for (let a = 1; a < values.length; a++) {
+
+                const nextString = values[a];
+                
+
+                if (nextString[b] != compare) {
+
+                    different = true;
+                    break;
+
+                    
+                }  
+            }
+
+            if (different == false) {
+
+                if (firstString[b] == "E") {
 
                     answer += "A'";
 
-                } else if (first[a] == "F") {
+                } else if (firstString[b] == "F") {
 
                     answer += "B'";
 
 
-                } else if (first[a] == "G") {
+                } else if (firstString[b] == "G") {
                     
                     answer += "C'";
 
 
-                } else if (first[a] == "H") {
+                } else if (firstString[b] == "H") {
 
                     answer += "D'";
 
 
                 } else {
 
-                    answer += first[a];
+                    answer += firstString[b];
 
                 }
             }
-        }
+
+            different = false;
     }
+
+}
 
     return answer;
 
   }
 
-  //coordinate = [1,2]
+  //coordinate = 0,1,2,3,4,5, etc.
   //numVariables = 3,4
   function equation(coordinate, numVariables) {
 
@@ -107,22 +149,86 @@
 
       const four = [
         ["EFGH", "EFGD", "EFCD", "EFCH"],
-        ["EBGH", "EBGD", "EBGD", "EBGH"],
+        ["EBGH", "EBGD", "EBCD", "EBCH"],
         ["ABGH", "ABGD", "ABCD", "ABCH"],
         ["AFGH", "AFGD", "AFCD", "AFCH"]
       ];
 
       if (numVariables == 3) {
 
-        return three[coordinate[0]][coordinate[1]];
+        if (coordinate == 0) {
+            return "EFG";
+          } else if (coordinate == 1) {
+            return "EFC";
+          } else if (coordinate == 2) {
+            return "EBG";
+          } else if (coordinate == 3) {
+            return "EBC";
+
+          } else if (coordinate == 4) {
+            return "AFG";
+          } else if (coordinate == 5) {
+            return "AFC";
+          } else if (coordinate == 6) {
+            return "ABG";
+          } else if (coordinate == 7) {
+            return "ABC";
+          }
 
 
-      } else if (numvariables == 4) {
+      } else if (numVariables == 4) {
 
-        return four[coordinate[0]][coordinate[1]];
+        if (coordinate == 0) {
+            return "EFGH";
+          } else if (coordinate == 1) {
+            return "EFGD";
+          } else if (coordinate == 2) {
+            return "EFCH";
+          } else if (coordinate == 3) {
+            return "EFCD";
 
+          } else if (coordinate == 4) {
+            return "EBGH";
+          } else if (coordinate == 5) {
+            return "EBGD";
+          } else if (coordinate == 6) {
+            return "EBCH";
+          } else if (coordinate == 7) {
+            return "EBCD";
+
+          } else if (coordinate == 8) {
+            return "ABGH";
+          } else if (coordinate == 9) {
+            return "ABGD";
+          } else if (coordinate == 10) {
+            return "ABCH";
+          } else if (coordinate == 11) {
+            return "ABCD";
+
+          } else if (coordinate == 12) {
+            return "AFGH";
+          } else if (coordinate == 13) {
+            return "AFGD";
+          } else if (coordinate == 14) {
+            return "AFCH";
+          } else if (coordinate == 15) {
+            return "AFCD";
+          }
+
+        
 
       }
-
-
   }
+
+  //TESTING, ADD COORDINATES HERE
+  //EX, 0,1,2,3,8,7,5
+        //EX, 1,2,3
+  const temp = [
+    
+        
+        7
+      
+    
+  ];
+
+console.log(findEquation(temp, 4));
