0b093a175c56e848cb3cf8c74c858b55bdd53c3d
Michael Hum <michaelhum28@gmail.com>
2023-11-18 01:23:15 -0500

    feat: WE'RE BACK

---
+++ alicehacks/src/Dropdown.jsx
@@ -0,0 +1,9 @@
+function Dropdown(){
+    return (
+    <div className ="relative flex flex-col w-[340px] h-[340px] rounded">
+        <button className ="bg-blue-400">Dropdown</button>
+    </div>
+    );
+}
+
+export default Dropdown;
