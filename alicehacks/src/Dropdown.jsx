d4839ca096cbb388f8e468e78815b834546ed9b2
Michael Hum <80639558+michaelhum28@users.noreply.github.com>
2023-11-18 04:16:31 -0500

    Delete alicehacks directory

--- alicehacks/src/Dropdown.jsx
+++
@@ -1,9 +0,0 @@
-function Dropdown(){
-    return (
-    <div className ="relative flex flex-col w-[340px] h-[340px] rounded">
-        <button className ="bg-blue-400">Dropdown</button>
-    </div>
-    );
-}
-
-export default Dropdown;
