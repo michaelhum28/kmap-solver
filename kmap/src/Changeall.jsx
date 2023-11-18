9a77867f549adeb9d6b878f8bb0c994e2b71f6eb
Michael Hum <michaelhum28@gmail.com>
2023-11-18 14:04:41 -0500

    feat: finished ui without connections to kmap

--- kmap/src/Changeall.jsx
+++ kmap/src/Changeall.jsx
@@ -7,25 +7,25 @@ function Changeall() {
       <div className="inline-flex rounded-md" role="group">
         <button
           type="button"
-          class="py-4 px-2.5 border-2 border-black mt-6 hover:ring-1 hover:ring-black rounded-s-lg"
+          class="py-4 px-3 border-2 border-black mt-6 hover:ring-1 hover:bg-gray-100 rounded-s-lg"
         >
           Set 1
         </button>
         <button
           type="button"
-          class="py-4 px-2.5 border-2 border-black mt-6 hover:ring-1 hover:ring-black"
+          class="py-4 px-3 border-2 border-y-black mt-6 hover:ring-1 hover:bg-gray-100"
         >
           Set x
         </button>
         <button
           type="button"
-          class="py-4 px-2.5 border-2 border-black mt-6 hover:ring-1 hover:ring-black rounded-e-lg"
+          class="py-4 px-3 border-2 border-black mt-6 hover:ring-1 hover:bg-gray-100 rounded-e-lg"
         >
           Clear
         </button>
       </div>
 
-      <button className="py-4 border-2 border-black mt-6 hover:ring-1 hover:ring-black">
+      <button className="py-4 border-2 border-black mt-6 hover:ring-1 hover:ring-black hover:bg-gray-100">
         Calculate
       </button>
     </div>
