5e08c3b365598506588cf8f1b4741a79f7e0b036
Michael Hum <michaelhum28@gmail.com>
2023-11-18 13:56:14 -0500

    feat: change all buttons finsihed

--- kmap/src/Changeall.jsx
+++ kmap/src/Changeall.jsx
@@ -3,23 +3,23 @@ import { useState } from "react";
 function Changeall() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex flex-col item">
-      <div className="inline-flex rounded-md shadow-sm" role="group">
+    <div className="flex flex-col">
+      <div className="inline-flex rounded-md" role="group">
         <button
           type="button"
-          class="px-4 py-2 bg-transparent border border-gray-600 rounded-s-lg hover:bg-gray-800 hover:text-white focus:z-10 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
+          class="py-4 px-2.5 border-2 border-black mt-6 hover:ring-1 hover:ring-black rounded-s-lg"
         >
           Set 1
         </button>
         <button
           type="button"
-          class="px-4 py-2 bg-transparent border-t border-b border-gray-600 hover:bg-gray-800 hover:text-white focus:z-10 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
+          class="py-4 px-2.5 border-2 border-black mt-6 hover:ring-1 hover:ring-black"
         >
           Set x
         </button>
         <button
           type="button"
-          class="px-4 py-2 bg-transparent border border-gray-600 rounded-e-lg hover:bg-gray-800 hover:text-white focus:z-10 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
+          class="py-4 px-2.5 border-2 border-black mt-6 hover:ring-1 hover:ring-black rounded-e-lg"
         >
           Clear
         </button>
