fd72a94c12b6f90f3b6febca0635571b7b241526
Michael Hum <michaelhum28@gmail.com>
2023-11-19 00:20:28 -0500

    feat: working number grids

--- kmap/src/Dropdown.jsx
+++ kmap/src/Dropdown.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 import list from "./list.json";
+import Grid from "./Grid"; // Import the Grid component
 
 function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,10 +13,24 @@ function Dropdown() {
     setIsOpen(false);
   };
 
+  const handleGenerateClick = () => {
+    // Perform any actions you need when the "Generate" button is clicked
+    if (selectedItem && selectedItem.total === "4x4") {
+      setGenerateClicked(true);
+    } else {
+      setGenerateClicked(false);
+    }
+  };
+
+  const handleDropdownClick = () => {
+    setIsOpen((prev) => !prev);
+    setGenerateClicked(false); // Reset generateClicked when dropdown is opened
+  };
+
   return (
     <div className="relative flex flex-col w-[240px] h-[240px] rounded">
       <button
-        onClick={() => setIsOpen((prev) => !prev)}
+        onClick={handleDropdownClick}
         className="w-full flex items-center justify-between rounded tracking-wider hover:bg-gray-100"
       >
         {"Variable Count: "}
@@ -30,6 +45,7 @@ function Dropdown() {
         )}
       </button>
 
+      {/* Render dropdown content only if isOpen is true */}
       {isOpen && (
         <div className="absolute top-10 flex flex-col items-start w-full">
           {list.map((item, i) => (
@@ -47,17 +63,21 @@ function Dropdown() {
 
       {/* Render a different button when an item is selected and the dropdown is closed */}
       {!isOpen && selectedItem && (
-        <button className="w-full mt-2 rounded tracking-wider hover:bg-gray-100">
+        <button
+          onClick={handleGenerateClick}
+          className="w-full mt-2 rounded tracking-wider hover:bg-gray-100"
+        >
           Generate
         </button>
       )}
 
-      {!isOpen && !selectedItem && (
+      {/* Render the Grid component only when generateClicked is true */}
+      {generateClicked && <Grid count={selectedItem.count} />}
+      
+      {!isOpen && !selectedItem && (  
         <button className="w-full mt-2 rounded tracking-wider hover:bg-gray-100 cursor-not-allowed opacity-50">
           Generate
         </button>
-
-        
       )}
     </div>
   );
--- kmap/src/Grid.jsx
+++ kmap/src/Grid.jsx
@@ -1,26 +1,24 @@
-function Grid (){
-    return(
-        <div className="grid grid-cols-4">
-  
-  <div className ="border-2 border-black w-[50px] h-[50px]">1</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">2</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">3</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">4</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">5</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">6</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">7</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">8</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">9</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">10</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">11</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">12</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">13</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">14</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">15</div>
-  <div className ="border-2 border-black w-[50px] h-[50px]">16</div>
-</div>
+function Grid({ count }) {
+  const generateGridItems = () => {
+    const gridItems = [];
+    for (let i = 0; i < count; i++) {
+      for (let j = 0; j < count; j++) {
+        const value = i * count + j;
+        gridItems.push(
+          <div key={value} className="grid grid-cols-4 border-2 border-black w-[50px] h-[50px]">
+            {value}
+          </div>
+        );
+      }
+    }
+    return gridItems;
+  };
 
-    );
-    
+  return (
+    <div className={`grid grid-cols-${count} p-6`}>
+      {generateGridItems()}
+      </div>
+  );
 }
+
 export default Grid;
